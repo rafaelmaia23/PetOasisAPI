f9f5fc0dc6f065e006d0bcd6b8aee3cab6210968
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:30:03 -0300

    Add login request dto

---
+++ Models/Auth/Dto/LoginRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetOasisAPI.Models.Auth.Dto;
+
+public class LoginRequestDto
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+}
