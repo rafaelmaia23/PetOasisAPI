b7e3adbffcc4535cc12d18c02f6afc1f797c86e0
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:28:15 -0300

    Add generic repository

---
+++ Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace PetOasisAPI.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>?> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>?> FindAsync(Expression<Func<T, bool>> expression);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task SaveAsync();
+    }
+}
