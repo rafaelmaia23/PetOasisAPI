e7e2c4a27181e8427d71b519d19e989b890933b7
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:27:26 -0300

    Add Generic Service

---
+++ Services/IServices/IService.cs
@@ -0,0 +1,15 @@
+﻿namespace PetOasisAPI.Services.IServices
+{
+    public interface IService<T, TReadDto, TCreateDto, TUpdateDto>
+        where T : class
+        where TReadDto : class
+        where TCreateDto : class
+        where TUpdateDto : class
+    {
+        Task<IEnumerable<TReadDto>> GetAllAsync();
+        Task<TReadDto> GetByIdAsync(int id);
+        Task<TReadDto> CreateAsync(TCreateDto dto);
+        Task<TReadDto> UpdateAsync(int id, TUpdateDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
