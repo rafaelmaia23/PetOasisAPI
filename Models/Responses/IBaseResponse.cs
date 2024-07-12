613be39796fe4fc664a41aa91173e4cdbb9706f9
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:28:33 -0300

    Add generic response

---
+++ Models/Responses/IBaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PetOasisAPI.Models.Responses;
+
+public interface IBaseResponse<T>
+{
+    bool Success { get; set; }
+    int StatusCode { get; set; }
+    string? Message { get; set; }
+    T? Data { get; set; }
+}
