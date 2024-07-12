b2bc44518b97c3ed69df6aa2831a18b5537a724b
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:29:12 -0300

    Add user repository

---
+++ Data/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace PetOasisAPI.Data.Repository.IRepository
+{
+    public interface IUserRepository<T> : IRepository<T> where T : class
+    {
+        Task<T?> GetByEmailAsync(string email);
+    }
+}
