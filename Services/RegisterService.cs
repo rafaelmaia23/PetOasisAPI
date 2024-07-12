4ba15428ea0820388f99f2a2696246f2e971b093
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-12 17:31:10 -0300

    Add register service

---
+++ Services/RegisterService.cs
@@ -0,0 +1,11 @@
+﻿using PetOasisAPI.Models.Responses;
+
+namespace PetOasisAPI.Services;
+
+public class RegisterService : BaseService<>, IResgisterService
+{
+    public RegisterResponse RegisterUser()
+    {
+        
+    }
+}
