cf485b3fdae57d0367d5cce6688f80f9f2a2f36c
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-09 18:22:59 -0300

    changed generation of employee number to a service

--- Models/Users/Employee.cs
+++ Models/Users/Employee.cs
@@ -1,36 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
-using PetOasisAPI.Data;
 
 namespace PetOasisAPI.Models.Users;
 
 public class Employee : AppUser
 {
-    private static int _currentEmployeeNumber = 0;
-
-    public static int CurrentEmployeeNumber 
-    { 
-        get { return _currentEmployeeNumber; }
-        private set { _currentEmployeeNumber = value; }
-    }
-
     [Required]
-    public string EmployeeNumber { get; set; }
+    public string EmployeeNumber { get; set; } = null!;
     [Required]
     public string Position { get; set; } = null!;
-
-    public Employee()
-    {
-        _currentEmployeeNumber++;
-        EmployeeNumber = GenerateEmployeeNumber();
-    }
-    private string GenerateEmployeeNumber()
-    {        
-        return $"{DateTime.Now.Year}{_currentEmployeeNumber:D3}";
-    }
-
-    public static async Task InitializeEmployeeNumberAsync(AppDbContext context)
-    {
-        _currentEmployeeNumber = await context.Employees.CountAsync();
-    }
 }
