183521cae6919bd3a45f62972caa5b36b98ce62b
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-19 16:08:47 -0300

    Change Nullability of Result

--- Models/Responses/APIResponse.cs
+++ Models/Responses/APIResponse.cs
@@ -11,5 +11,5 @@ public class APIResponse
     public HttpStatusCode StatusCode { get; set; }
     public bool Success { get; set; }
     public List<string> Messages { get; set; }
-    public object Result { get; set; }
+    public object? Result { get; set; }
 }
