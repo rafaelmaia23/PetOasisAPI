51d8855a4908d005854e63784aad582000107376
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-19 17:21:49 -0300

    Create AppUserDto

---
+++ Models/Dtos/Users/AppUserDto.cs
@@ -0,0 +1,10 @@
+namespace PetOasisAPI.Models.Dtos.Users;
+
+public class AppUserDto
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+}
