23546994d4e43587c7f58b636f08abd5e6d3a8ca
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-20 17:37:25 -0300

    Fixed return status and add metadata of returns and auth

--- Routes/Auth/RegisterRoutes.cs
+++ Routes/Auth/RegisterRoutes.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using PetOasisAPI.Attributes;
 using PetOasisAPI.Models.Auth;
 using PetOasisAPI.Models.Responses;
@@ -14,27 +15,40 @@ public static class RegisterRoutes
         {
             var result = await service.RegisterTutorAsync(request);
 
-            return new APIResponse()
+            var response = new APIResponse()
             {
                 StatusCode = (HttpStatusCode)result.StatusCode,
                 Success = true,
                 Messages = result.Messages,
                 Result = result.Data
             };
-        });
+
+            if(!result.Success) return Results.BadRequest(response);
+            return Results.Ok(response);
+        })
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status200OK))
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
 
         app.MapPost("/registerEmployee", [ModelType(typeof(RegisterEmployeeRequest))] async (RegisterEmployeeRequest request, IRegisterService service) =>
         {
             var result = await service.RegisterEmployeenAsync(request);
 
-            return new APIResponse()
+            var response = new APIResponse()
             {
                 StatusCode = (HttpStatusCode)result.StatusCode,
                 Success = true,
                 Messages = result.Messages,
                 Result = result.Data
             };
-        });
+
+            if(!result.Success) return Results.BadRequest(response);
+            return Results.Ok(response);
+        })
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status200OK))
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest))
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status401Unauthorized))
+        .WithMetadata(new ProducesResponseTypeAttribute(StatusCodes.Status403Forbidden))
+        .RequireAuthorization(policy => policy.RequireRole("admin"));
     }
 
     
