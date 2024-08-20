82aaef2cf65a60a4d2aa77c04b2e4b8e6a6847b4
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-20 17:38:49 -0300

    change class to record

--- Models/Auth/UserSession.cs
+++ Models/Auth/UserSession.cs
@@ -1,9 +1,8 @@
 namespace PetOasisAPI.Models.Auth;
 
-public class UserSession
-{
-    public string Id { get; set; } = null!;
-    public string Name { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Role { get; set; } = null!;
-}
+public record UserSession
+(   string Id,
+    string UserName,
+    string Email,
+    string Role
+);
