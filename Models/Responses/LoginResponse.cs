7472c62e2bac63560024c774cba54776ff04c519
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-08-20 17:39:45 -0300

    change Data type from AppUser to AppUserDto

--- Models/Responses/LoginResponse.cs
+++ Models/Responses/LoginResponse.cs
@@ -1,13 +1,13 @@
-﻿using PetOasisAPI.Models.Users;
+﻿using PetOasisAPI.Models.Dtos.Users;
 
 namespace PetOasisAPI.Models.Responses
 {
-    public class LoginResponse : IBaseServiceResponse<AppUser>
+    public class LoginResponse : IBaseServiceResponse<AppUserDto>
     {        
         public bool Success { get; set; }
         public int StatusCode { get; set; }
         public List<string>? Messages { get; set; }
-        public AppUser? Data { get; set; }
+        public AppUserDto? Data { get; set; }
         public string Token { get; set; } = null!;
     }
 }
